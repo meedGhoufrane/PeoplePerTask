eca2c952e44bdd5abe6b7993246693af0e1d9dfc
MedELHattab <elhattabmohammedelarbi.com>
2023-11-02 17:33:47 +0100

    This is some modifications I made in my code

--- js/about-contact.js
+++
@@ -1,8 +0,0 @@
-// JavaScript to toggle dark mode
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('click', function () {
-  body.classList.toggle('dark-mode');
-  // You can also store the user's preference in a cookie or local storage.
-});
